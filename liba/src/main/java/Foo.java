6c5fcb641f523549867d5bc0cf951ddcd009b6dd
Liangchen <leeonky@gmail.com>
2019-05-08 23:14:37 +0800

    Test test coverage

--- liba/src/main/java/Foo.java
+++ liba/src/main/java/Foo.java
@@ -2,8 +2,4 @@ public class Foo {
     public String fun() {
         return "Hello world";
     }
-
-    public String fun2() {
-        return "Hello world";
-    }
 }
--- libb/src/main/java/Foo2.java
+++ libb/src/main/java/Foo2.java
@@ -2,4 +2,8 @@ public class Foo2 {
     public String fun() {
         return new Foo().fun();
     }
+
+    public String fun2() {
+        return "Hello world";
+    }
 }
